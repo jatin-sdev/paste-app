efa39e13dd7fce8d338e61158ca5b21ad72e0300
jatin-sdev <jatin.sdev@gmail.com>
2025-10-14 23:00:42 +0530

    test

--- paste-app/src/components/Home.jsx
+++ paste-app/src/components/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useSearchParams} from 'react-router-dom'
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const [searchParams, setSearchParams] = useSearchParams("");
+
+  const pasteId = searchParams.get("pasteId");
+
+  return ( 
+    <div className='Home'>
+      <input className='Input' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Enter Title' />
+
+      <button className='Button'>{pasteId ? "Update Paste" : "Create Paste"}</button>
+    </div>
   )
 }
 
